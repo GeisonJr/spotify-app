14e62ef3e41bd013c30e695c16c4a33695a03db7
Geison Oriani <eu@geison.dev>
2025-08-05 23:40:08 -0300

    refactor: update Vite configuration for improved plugin usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
+import tanstackRouter from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: 'src/routes'
+    }),
+    react()
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+      '@': '/src'
+    }
+  }
 })
