1bde9bd0296d522e2152105c632aa7129c6c2799
Geison Oriani <eu@geison.dev>
2025-08-06 19:55:33 -0300

    feat: enhance Profile component with user data handling and avatar display logic

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -1,26 +1,40 @@
 import Layout from '@/components/Layout'
 import { createFileRoute, Link } from '@tanstack/react-router'
 
+import If from '@/components/If'
 import link from '@/components/Link/index.module.css'
+import type { UserResponse } from '@/types'
 import styles from './index.module.css'
 
 export const Route = createFileRoute('/profile/')({
+  loader: async () => {
+    return { user }
+  },
   component: Profile
 })
 
 function Profile() {
 
+  const { user } = Route.useLoaderData()
+
   return (
     <>
       <Layout>
         <div className={styles.content}>
-          <img
-            className={styles.avatar}
-            alt={'Avatar'}
-            src={'/avatar.png'}
-          />
+          <If condition={user.data.images.length === 0}>
+            <div className={styles.noAvatar}>
+              {user.data.display_name[0]}
+            </div>
+          </If>
+          <If condition={user.data.images.length > 0}>
+            <img
+              className={styles.avatar}
+              alt={'Avatar'}
+              src={user.data.images[0]?.url}
+            />
+          </If>
           <h2 className={styles.username}>
-            {'Nome do Usuário'}
+            {user.data.display_name}
           </h2>
           <Link className={link.container} to={'/'}>
             {'Sair'}
@@ -29,4 +43,30 @@ function Profile() {
       </Layout>
     </>
   )
+}
+
+const user: UserResponse = {
+  message: 'Usuário carregado com sucesso',
+  data: {
+    "country": "BR",
+    "display_name": "GeisonJr",
+    "email": "eu+spotify@geison.dev",
+    "explicit_content": {
+      "filter_enabled": false,
+      "filter_locked": false
+    },
+    "external_urls": {
+      "spotify": "https://open.spotify.com/user/geisonjr"
+    },
+    "followers": {
+      "href": null,
+      "total": 6
+    },
+    "href": "https://api.spotify.com/v1/users/geisonjr",
+    "id": "geisonjr",
+    "images": [],
+    "product": "premium",
+    "type": "user",
+    "uri": "spotify:user:geisonjr"
+  }
 }
