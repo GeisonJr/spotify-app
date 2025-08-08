df66b6272bb73e27a6cf20e1f15f2dd28030882c
Geison Oriani <eu@geison.dev>
2025-08-07 23:41:43 -0300

    refactor: consolidate image styles and improve no-image handling in PlaylistCard

--- src/components/PlaylistCard/index.tsx
+++ src/components/PlaylistCard/index.tsx
@@ -1,16 +1,26 @@
+import If from '../If'
 import styles from './index.module.css'
 import type { PlaylistCardProps } from './types'
 
 function PlaylistCard({ playlist }: PlaylistCardProps) {
 
+  const length = playlist.images?.length ?? 0
+
   return (
     <>
       <div className={styles.container}>
-        <img
-          className={styles.image}
-          alt={'Playlist'}
-          src={playlist.images[0].url}
-        />
+        <If condition={length === 0}>
+          <div className={styles.noImage}>
+            {playlist.name[0]}
+          </div>
+        </If>
+        <If condition={length > 0}>
+          <img
+            className={styles.image}
+            alt={'Playlist'}
+            src={playlist.images?.[0].url}
+          />
+        </If>
         <div className={styles.content}>
           <p className={styles.title}>
             {playlist.name}
