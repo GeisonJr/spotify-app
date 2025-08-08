e6922458de315d542478e1b386a366e7d7642b40
Geison Oriani <eu@geison.dev>
2025-08-07 23:41:02 -0300

    refactor: consolidate image styles and improve no-image handling in AlbumCard

--- src/components/AlbumCard/index.tsx
+++ src/components/AlbumCard/index.tsx
@@ -1,16 +1,26 @@
+import If from '../If'
 import styles from './index.module.css'
 import type { AlbumCardProps } from './types'
 
 function AlbumCard({ album }: AlbumCardProps) {
 
+  const length = album.images?.length ?? 0
+
   return (
     <>
       <div className={styles.container}>
-        <img
-          className={styles.image}
-          alt={'Album'}
-          src={album.images[0].url}
-        />
+        <If condition={length === 0}>
+          <div className={styles.noImage}>
+            {album.name[0]}
+          </div>
+        </If>
+        <If condition={length > 0}>
+          <img
+            className={styles.image}
+            alt={'Album'}
+            src={album.images?.[0].url}
+          />
+        </If>
         <div className={styles.content}>
           <p className={styles.title}>
             {album.name}
