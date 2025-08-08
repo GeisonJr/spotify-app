a2df1a5c87b60c36f1785c43b3122626b201cbee
Geison Oriani <eu@geison.dev>
2025-08-07 23:44:00 -0300

    refactor: enhance fetcher function to support query parameters in GET and POST requests

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,7 +1,17 @@
-async function fetcher(url: string, options?: RequestInit) {
+interface Options extends RequestInit {
+  params?: Record<string, any>
+}
+
+async function fetcher(url: string, options?: Options) {
 
   const apiUrl = new URL('http://127.0.0.1:3000/')
   apiUrl.pathname = url
+
+  const params = options?.params
+  if (params) {
+    const searchParams = new URLSearchParams(params)
+    apiUrl.search = searchParams.toString()
+  }
 
   return await fetch(apiUrl, {
     credentials: 'include',
@@ -13,14 +23,14 @@ async function fetcher(url: string, options?: RequestInit) {
   })
 }
 
-export function get(url: string, options?: Omit<RequestInit, 'method'>) {
+export function get(url: string, options?: Omit<Options, 'method'>) {
   return fetcher(url, {
     ...options,
     method: 'GET'
   })
 }
 
-export function post(url: string, options?: Omit<RequestInit, 'method'>) {
+export function post(url: string, options?: Omit<Options, 'method'>) {
   return fetcher(url, {
     ...options,
     method: 'POST'
