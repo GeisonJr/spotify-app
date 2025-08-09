0312407fb79b1de332f5849b831f53a34b03b2a8
Geison Oriani <eu@geison.dev>
2025-08-09 15:58:50 -0300

    feat: add modal for creating playlists with form handling and state management

--- src/routes/playlists/index.tsx
+++ src/routes/playlists/index.tsx
@@ -1,11 +1,15 @@
 import If from '@/components/If'
 import link from '@/components/Link/index.module.css'
 import Map from '@/components/Map'
+import Modal from '@/components/Modal'
 import PlaylistCard from '@/components/PlaylistCard'
 import ScrollableLayout from '@/components/ScrollableLayout'
+import { post } from '@/functions'
 import { useInfinitePlaylists } from '@/hooks/useInfinitePlaylists'
+import type { Playlist, PlaylistCreateResponse, PlaylistResponse } from '@/types'
+import { useQueryClient, type InfiniteData } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import styles from './index.module.css'
 
 export const Route = createFileRoute('/playlists/')({
@@ -13,6 +17,11 @@ export const Route = createFileRoute('/playlists/')({
 })
 
 function Playlists() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const queryClient = useQueryClient()
+
   const {
     data,
     status,
@@ -27,12 +36,78 @@ function Playlists() {
 
   const isLoading = status === 'pending'
 
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const name = formData.get('playlist-name') as string
+
+    if (!name) {
+      alert('Por favor, insira um nome para a playlist.')
+      return
+    }
+
+    const response = await post('/playlist/me', {
+      body: JSON.stringify({
+        name
+      })
+    })
+
+    if (!response.ok) {
+      alert('Erro ao criar a playlist. Tente novamente.')
+      return
+    }
+
+    const newPlaylist = await response.json() as PlaylistCreateResponse
+
+    const optimisticPlaylist: Playlist = {
+      ...newPlaylist.data,
+      primary_color: null
+    }
+
+    // Atualiza o cache imediatamente
+    queryClient.setQueryData<InfiniteData<PlaylistResponse>>(['playlists'], (oldData) => {
+      if (!oldData) {
+        return oldData
+      }
+
+      return {
+        ...oldData,
+        pages: oldData.pages.map((page, index) => {
+          if (index === 0) {
+            return {
+              ...page,
+              data: {
+                ...page.data,
+                items: [
+                  optimisticPlaylist,
+                  ...page.data.items
+                ]
+              }
+            }
+          }
+          return page
+        })
+      }
+    })
+
+    setIsModalOpen(false)
+  }
+
   return (
     <>
       <ScrollableLayout
         header={{
           title: 'Minhas Playlists',
-          description: 'Sua coleção pessoal de playlists'
+          description: 'Sua coleção pessoal de playlists',
+          children: (
+            <button
+              className={link.container}
+              onClick={() => setIsModalOpen(true)}
+            >
+              {'Criar Playlist'}
+            </button>
+          )
         }}
       >
         <div className={styles.content}>
@@ -62,6 +137,33 @@ function Playlists() {
           </div>
         </div>
       </ScrollableLayout>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+        <form
+          className={styles.modal}
+          onSubmit={handleSubmit}
+        >
+          <div className={styles.modalContent}>
+            <label htmlFor={'playlist-name'}>
+              {'Dê um nome a sua playlist'}
+            </label>
+            <input
+              autoFocus
+              id={'playlist-name'}
+              name={'playlist-name'}
+            />
+          </div>
+          <button
+            className={link.container}
+            type={'submit'}
+          >
+            {'Criar'}
+          </button>
+        </form>
+      </Modal>
     </>
   )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -88,7 +88,7 @@ export type ArtistResponse = ResponseWithPagination<{
 }>
 
 export interface Playlist {
-  collaborative: boolean,
+  collaborative: boolean
   description: string
   external_urls: {
     spotify: string
@@ -104,19 +104,19 @@ export interface Playlist {
   owner: {
     external_urls: {
       spotify: string
-    },
+    }
     href: string
     id: string
     type: string
     uri: string
     display_name: string
-  },
-  public: boolean,
+  }
+  public: boolean
   snapshot_id: string
   tracks: {
     href: string
     total: number
-  },
+  }
   type: string
   uri: string
   primary_color: string | null
@@ -131,6 +131,102 @@ export type PlaylistResponse = ResponseWithPagination<{
   total: number
   items: Playlist[]
 }>
+
+export interface PlaylistCreate {
+  collaborative: boolean
+  description: string
+  external_urls: {
+    spotify: string
+  }
+  href: string
+  id: string
+  images: {
+    url: string
+    height: number
+    width: number
+  }[]
+  name: string
+  owner: {
+    external_urls: {
+      spotify: string
+    }
+    href: string
+    id: string
+    type: string
+    uri: string
+    display_name: string
+  }
+  public: boolean
+  snapshot_id: string
+  tracks: {
+    href: string
+    limit: number
+    next: string
+    offset: number
+    previous: string
+    total: number
+    items: {
+      added_at: string
+      added_by: {
+        external_urls: {
+          spotify: string
+        }
+        href: string
+        id: string
+        type: string
+        uri: string
+      }
+      is_local: boolean
+      track: {
+        album: Album
+        artists: {
+          external_urls: {
+            spotify: string
+          }
+          href: string
+          id: string
+          name: string
+          type: string
+          uri: string
+        }[]
+        available_markets: string[]
+        disc_number: number
+        duration_ms: number
+        explicit: boolean
+        external_ids: {
+          isrc: string
+          ean: string
+          upc: string
+        }
+        external_urls: {
+          spotify: string
+        }
+        href: string
+        id: string
+        is_playable: boolean
+        linked_from: {
+        }
+        restrictions: {
+          reason: string
+        }
+        name: string
+        popularity: number
+        preview_url: string
+        track_number: number
+        type: string
+        uri: string
+        is_local: boolean
+      }
+    }[]
+  }
+  type: string
+  uri: string
+}
+
+export interface PlaylistCreateResponse {
+  message: string
+  data: PlaylistCreate
+}
 
 export interface User {
   country: string
@@ -154,7 +250,7 @@ export interface User {
     url: string
     height: number
     width: number
-  }[],
+  }[]
   product: string
   type: string
   uri: string
